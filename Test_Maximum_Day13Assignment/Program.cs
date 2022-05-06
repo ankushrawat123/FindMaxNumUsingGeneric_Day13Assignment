4679270e6fc2c8720e22d05bdbf062b180f72302
AnkushRawat <ankushrwt344@gmail.com>
2022-05-07 00:06:43 +0530

    Uploading MaxNumOut of 5 Using Generic file in Uc4

--- Test_Maximum_Day13Assignment/Program.cs
+++ Test_Maximum_Day13Assignment/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using FindMaxNumUsingGeneric_Day13Assignment;
 
-Console.WriteLine("Enter \n1 For FIND_MAX_Int_NUMBER_WITHOUT_GENERIC PROGRAM \n2 For FIND_MAX_FLOAT_NUMBER_WITHOUT_GENERIC PROGRAM \n3 For FIND_MAX_STRING_NUMBER_WITHOUT_GENERIC PROGRAM\n");
+Console.WriteLine("Enter \n1 For FIND_MAX_Int_NUMBER_WITHOUT_GENERIC PROGRAM \n2 For FIND_MAX_FLOAT_NUMBER_WITHOUT_GENERIC PROGRAM \n3 For FIND_MAX_STRING_NUMBER_WITHOUT_GENERIC PROGRAM\n4 For FIND MAX_OUT_OF_MORE_THAN_THREE_NUMBER_USING_GENERIC PROGRAM \n");
 int num = Convert.ToInt32(Console.ReadLine());
 switch (num)
 {
@@ -16,5 +16,18 @@ switch (num)
     case 3:
         Console.WriteLine("\nWelcome to FIND_MAX_STRING_WITHOUT_GENERIC PROGRAM \n");
         Console.WriteLine("Maximum String : " + StringMaxWithoutGenerics.MaximumStringNumber("Ankush","Lokesh","Jeet"));
+        break;
+    case 4:
+        Console.WriteLine("\nWelcome to FIND MAX_OUT_OF_MORE_THAN_THREE_NUMBER_USING_GENERIC PROGRAM \n");
+
+        int[] intArray = { 100, 531, 432, 89, 34 };
+        GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+        generic.PrintMaxValue();
+        double[] doubleArray = { 85.5, 71.9, 35.4, 45.2, 67.8 };
+        GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
+        genericDouble.PrintMaxValue();
+        string[] stringArray = { "Ankush", "Lokesh", "Jeet", "Himanshu", "Kalauddin" };
+        GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
+        genericString.PrintMaxValue();
         break;
 }
