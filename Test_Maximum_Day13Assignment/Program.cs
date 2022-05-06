c0c28e0b6d5d0aaffeb41a967cba5d2337ff3edd
AnkushRawat <ankushrwt344@gmail.com>
2022-05-06 22:58:02 +0530

    Uploading StringMaxFindingWithoutGenerics program in Uc3

--- Test_Maximum_Day13Assignment/Program.cs
+++ Test_Maximum_Day13Assignment/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using FindMaxNumUsingGeneric_Day13Assignment;
 
-Console.WriteLine("Enter \n1 For FIND_MAX_Int_NUMBER_WITHOUT_GENERIC PROGRAM \n2 For FIND_MAX_FLOAT_NUMBER_WITHOUT_GENERIC PROGRAM");
+Console.WriteLine("Enter \n1 For FIND_MAX_Int_NUMBER_WITHOUT_GENERIC PROGRAM \n2 For FIND_MAX_FLOAT_NUMBER_WITHOUT_GENERIC PROGRAM \n3 For FIND_MAX_STRING_NUMBER_WITHOUT_GENERIC PROGRAM\n");
 int num = Convert.ToInt32(Console.ReadLine());
 switch (num)
 {
@@ -12,5 +12,9 @@ switch (num)
     case 2:
         Console.WriteLine("\nWelcome to FIND_MAX_FLOAT_NUMBER_WITHOUT_GENERIC PROGRAM \n");
         Console.WriteLine("Maximum Float Number : " + FloatMaxNumWithoutGenerics.MaximumFloatNumber(85.5, 71.9, 35.4));
+        break;
+    case 3:
+        Console.WriteLine("\nWelcome to FIND_MAX_STRING_WITHOUT_GENERIC PROGRAM \n");
+        Console.WriteLine("Maximum String : " + StringMaxWithoutGenerics.MaximumStringNumber("Ankush","Lokesh","Jeet"));
         break;
 }
