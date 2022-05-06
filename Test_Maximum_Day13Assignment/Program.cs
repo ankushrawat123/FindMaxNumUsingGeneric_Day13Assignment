83b17b52614fea3a62dd2c0a3544ae0cbf0f156a
AnkushRawat <ankushrwt344@gmail.com>
2022-05-06 18:37:54 +0530

    Add project files.

---
+++ Test_Maximum_Day13Assignment/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FindMaxNumUsingGeneric_Day13Assignment;
+Console.WriteLine("Welcome to FIND_MAX_NUMBER_USING_GENERICS PROGRAM \n");
+int[] intArray = { 100, 531, 432 };
+GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+generic.PrintMaxValue();
