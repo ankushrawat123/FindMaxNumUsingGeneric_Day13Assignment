384cb9b3bcd1c82732d16836cdf6c52a1758c92c
AnkushRawat <ankushrwt344@gmail.com>
2022-05-10 22:52:03 +0530

    uploading

--- Test_Maximum_Day13Assignment/Program.cs
+++ Test_Maximum_Day13Assignment/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using FindMaxNumUsingGeneric_Day13Assignment;
 
-Console.WriteLine("Enter \n1 For FIND_MAX_Int_NUMBER_WITHOUT_GENERIC PROGRAM \n2 For FIND_MAX_FLOAT_NUMBER_WITHOUT_GENERIC PROGRAM \n3 For FIND_MAX_STRING_NUMBER_WITHOUT_GENERIC PROGRAM\n4 For FIND MAX_OUT_OF_MORE_THAN_THREE_NUMBER_USING_GENERIC PROGRAM \n");
+Console.WriteLine("Enter \n1 For FIND_MAX_Int_NUMBER_WITHOUT_GENERIC PROGRAM \n2 For FIND_MAX_FLOAT_NUMBER_WITHOUT_GENERIC PROGRAM \n3 For FIND_MAX_STRING_NUMBER_WITHOUT_GENERIC PROGRAM\n4 For FIND MAX_OUT_OF_MORE_THAN_THREE_NUMBER_USING_GENERIC PROGRAM \n5 Welcome to REFACTOR PROGRAM TO FIND_MAX_INT_DOUBLE_STRING_NUMBER WITH GENERICS\n");
 int num = Convert.ToInt32(Console.ReadLine());
 switch (num)
 {
@@ -9,14 +9,17 @@ switch (num)
         Console.WriteLine("\nWelcome to FIND_MAX_Int_NUMBER_WITHOUT_GENERIC PROGRAM \n");
         Console.WriteLine("Maximum Int Number : " + IntegerMaxNumber.MaximumIntegerNumber(100, 531, 432));
         break;
+
     case 2:
         Console.WriteLine("\nWelcome to FIND_MAX_FLOAT_NUMBER_WITHOUT_GENERIC PROGRAM \n");
         Console.WriteLine("Maximum Float Number : " + FloatMaxNumWithoutGenerics.MaximumFloatNumber(85.5, 71.9, 35.4));
         break;
+
     case 3:
         Console.WriteLine("\nWelcome to FIND_MAX_STRING_WITHOUT_GENERIC PROGRAM \n");
         Console.WriteLine("Maximum String : " + StringMaxWithoutGenerics.MaximumStringNumber("Ankush","Lokesh","Jeet"));
         break;
+
     case 4:
         Console.WriteLine("\nWelcome to FIND MAX_OUT_OF_MORE_THAN_THREE_NUMBER_USING_GENERIC PROGRAM \n");
 
@@ -46,6 +49,15 @@ switch (num)
         Console.WriteLine("\nArray String Value After Sorting");
         genericString.display();
         genericString.PrintMaxValue();
-
         break;
-}
+
+    case 5:
+        Console.WriteLine("\nWelcome to REFACTOR PROGRAM TO FIND_MAX_INT_DOUBLE_STRING_NUMBER WITH GENERICS\n");
+        Refactor<int> intObj = new Refactor<int>(100, 531, 432);
+        Console.WriteLine("Maximum Int Number: " + intObj.MaxValue(100, 531, 432));
+        Refactor<double> doubleObj = new Refactor<double>(55.5, 43.2, 498.5);
+        Console.WriteLine("Maximum Double Number: " + doubleObj.MaxValue(55.5, 43.2, 498.5));      
+        Refactor<string> stringObj = new Refactor<string>("Ankush", "Jeet", "Kalauddin");
+        Console.WriteLine("Maximum String Number: " + stringObj.MaxValue("Ankush", "Jeet", "Kalauddin"));
+        break;
+}   
